018f8a3ceee78d334ec2b09172773059b3b33709
Brahma <brahmaborude2005@gmail.com>
2025-02-04 17:13:25 +0530

    signup page done

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path("signin/", views.SignIn, name="signin"),
     path("", views.SignUp, name="signup"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 def SignIn(request):
-    return render(request, "signin.html")
+    return render(request, "users/signin.html")
 
 def SignUp(request):
-    return render(request, "signup.html")
+    return render(request, "users/signup.html")
